728cfb0f32068ba53b435ee343439f15436f6a00
masonfibkins <35748689+masonfibkins@users.noreply.github.com>
2020-10-12 13:52:42 -0600

    Add files via upload

--- tone_lab.ino
+++ tone_lab.ino
@@ -16,7 +16,7 @@
 */
 
 #include "pitches.h"
-
+char val;
 // notes in the melody:
 int melody[] = {
   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
@@ -28,14 +28,26 @@ int noteDurations[] = {
 };
 
 void setup() {
-  // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  pinMode(9, OUTPUT); // Set pin as OUTPUT
+ Serial.begin(9600); // Start serial communication at 9600 bps
+
+
+}
+
+void loop() {
+  // no need to repeat the melody.
+  while (Serial.available()) { // If data is available to read,
+ val = Serial.read(); // read it and store it in val
+ }
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    
     int noteDuration = 1000 / noteDurations[thisNote];
+    if(val=='H'){
     tone(9, melody[thisNote], noteDuration);
-
+    }
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
@@ -43,8 +55,4 @@ void setup() {
     // stop the tone playing:
     noTone(9);
   }
-}
-
-void loop() {
-  // no need to repeat the melody.
 }
